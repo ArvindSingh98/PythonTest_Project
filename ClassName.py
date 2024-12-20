55d7bfd77947087f41dbede3c3c0c444bc2b84fd
ArvindSingh98 <johndoe@example.comarvind.singh@ufomoviez.com>
2024-12-20 15:49:47 +0530

    changes of class name file

--- ClassName.py
+++ ClassName.py
@@ -1,32 +1,26 @@
-import Models.ErrorLog as Logger 
+import Models.ErrorLog as Logger
 
-
-
-class A:    
-    
+class A:
     def __init__(self):
-        print('Object Initialize!!')        
-        
+        print('Object Initialize!!')
 
-    def func_PrintDivision(self):
+    @staticmethod
+    def func_PrintDivision():
         try:
-            self.TxtMsg = eval(input('Enter Num to display the Division Result:'))
-            self.Div = eval(input('Enter Num to Divide the entered Num:'))
-            if(self.TxtMsg is not None ):
-                #Logger.func_Print('Printed')
-                print('Entered No Division Is :',(self.TxtMsg)/self.Div)
+            TxtMsg = eval(input('Enter Num to display the Division Result:'))
+            Div = eval(input('Enter Num to Divide the entered Num:'))
+            if TxtMsg is not None:
+                # Logger.func_Print('Printed')
+                print('Entered No Division Is :', (TxtMsg) / Div)
             else:
                 print('Nothing to display!')
         except Exception as err:
-             # print('Error Occured As :')
-             #print(logging.error(err, exc_info=True))
-             print(Logger.func_logError(err))
-
+            # print('Error Occured As :')
+            # print(logging.error(err, exc_info=True))
+            print(Logger.func_logError(err))
 
 if __name__ == "__main__":
-
-    objA = A()
-    objA.func_PrintDivision()
+    A().func_PrintDivision()
 
 
 
